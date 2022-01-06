1e9a409bb6b719ce5fb7b8d17147a1a8d24b3d1e
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-01-06 20:31:34 +0200

    Fixed simplelightbox

--- src/index.js
+++ src/index.js
@@ -21,58 +21,66 @@ refs.form.addEventListener('submit', fetchSubmit)
 
 
 let counter = 1;
+let gallerySet = new SimpleLightbox('.gallery a');
+
 function fetchSubmit(e) {
   e.preventDefault();
-    refs.divCard.innerHTML = '';
     // disabledBtn();
   const searchName = e.target.elements.searchQuery.value
-  createRender(searchName);
+  
   window.addEventListener('scroll', () => {
     let contentHeight = refs.divCard.offsetHeight; 
 let Offset = window.pageYOffset;
 let window_height = window.innerHeight;
-let y = Offset + window_height;
-      if (y >= contentHeight) {
-        counter += 1;
+    let y = Offset + window_height;
+    if (y >= contentHeight) {
+      gallerySet.refresh();
+      
+      counter += 1;
         createRenderScroll(searchName, counter);
   }
   })
+  refs.divCard.innerHTML = '';
+  createRender(searchName);
+  counter = 1;
 }
 
 
 
 
 async function createRender(searchName) {
-  console.log(counter);
-   await axios.get(`${BASE_URL}?key=${KEY}&q=${searchName}&${PARAMS_SEARCH}&page=1`)
-     .then(result => {
-        if (result.data.hits.length < 1) {
+
+  await axios.get(`${BASE_URL}?key=${KEY}&q=${searchName}&${PARAMS_SEARCH}&page=${counter}`)
+    .then(result => {
+      if (result.data.hits.length < 1) {
         Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.')
         return;
-        }
-        //  enambeBtn();
-       const Obj = result.data.hits;
-        Notiflix.Notify.success(`Hooray! We found ${result.data.totalHits} images.`)
-         reqListener(Obj);
+      }
+      //  enambeBtn();
+      const Obj = result.data.hits;
+      Notiflix.Notify.success(`Hooray! We found ${result.data.totalHits} images.`)
+      reqListener(Obj);
             
-        });
+    });
 }
 
 async function createRenderScroll(searchName, counter) {
   console.log(counter);
-   await axios.get(`${BASE_URL}?key=${KEY}&q=${searchName}&${PARAMS_SEARCH}&page=${counter}`)
-     .then(result => {
-        if (result.data.hits.length < 1) {
+
+  await axios.get(`${BASE_URL}?key=${KEY}&q=${searchName}&${PARAMS_SEARCH}&page=${counter}`)
+    .then(result => {
+      if (result.data.hits.length < 1) {
         Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.')
         return;
-        }
-        //  enambeBtn();
-            const Obj = result.data.hits;
-         reqListener(Obj);
+      }
+      //  enambeBtn();
+      const Obj = result.data.hits;
+      reqListener(Obj);
             
-        });
+    });
 }
-async function reqListener(Obj) {
+ function reqListener(Obj) {
+  
   const addCards = Obj.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
       return `<div class="photo-card">
      <a class="gallery__item" href='${largeImageURL}'>
@@ -98,10 +106,15 @@ async function reqListener(Obj) {
   </div>
 </div>`
      })
-    refs.divCard.insertAdjacentHTML('beforeend', addCards.join(''));
+  refs.divCard.insertAdjacentHTML('beforeend', addCards.join(''));
+  gallerySet.refresh();
+
+    
+  
+  
   // enambeBtn();
-  let gallerySet = new SimpleLightbox('.gallery a');
-    gallerySet.refresh();
+  
+    
 }
 
 
@@ -133,5 +146,4 @@ async function reqListener(Obj) {
 //     refs.loadMore.textContent = 'Load more'
 //     refs.loadMore.disabled = false;
 // }
-
 
